565981e97bcad0161a55e424a9af54c77d08cafa
Nisha Chandramoorthy <nishac92@gmail.com>
2018-08-25 00:05:02 -0400

    Write to file, ready to run on the cluster

--- driver_sensitivity.jl
+++ driver_sensitivity.jl
@@ -1,19 +1,21 @@
 @everywhere include("Lorenz63.jl")
 @everywhere include("ensemble_sensitivity.jl")
-tau = 85
-T = 5000
-N = 100
-n_estimators = Int(T/N)
-
+using DelimitedFiles
+TT = 5000000
+T = 100000
 ds = [0.,1.0,0.]
 s = copy(s0)
-n_tau = 4
-tau_arr = range(60, step=10, length=n_tau)
+n_tau = 40 
+tau_arr = range(30, step=5, length=n_tau)
 
 exp_theta_tauN = zeros(n_tau)
 var_theta_tauN = zeros(n_tau)
 
 for (tau_ind, tau) in enumerate(tau_arr)
+	N = floor(Int,(T/tau))
+	n_estimators = floor(Int,(TT/N))
+
+		
 		exp_theta_tauN[tau_ind] = @sync @distributed (+) for i = 1:T
 			compute_tangent_sensitivity(rand(3),
 										s,tau,ds)/T
@@ -32,6 +34,11 @@ for (tau_ind, tau) in enumerate(tau_arr)
 		
 end
 
-
+open("expected_value_tangent.txt","w") do io
+		writedlm(io, exp_theta_tauN)
+end
+open("variance_tangent.txt","w") do io
+		writedlm(io, var_theta_tauN)
+end
 
 
