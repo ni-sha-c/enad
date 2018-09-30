ba508dc2cfab0de0a2481acb2ccce78456e6c9ef
Nisha Chandramoorthy <nishac92@gmail.com>
2018-09-29 20:15:39 -0400

    Still giving nans

--- compute_CLVs.jl
+++ compute_CLVs.jl
@@ -2,32 +2,37 @@
 @everywhere include("Lorenz63.jl")
 @everywhere using SharedArrays
 function compute_CLVs(u0::Array{Float64,1})
-	n_dim = length(u0)
+    u0 = Step(rand(3),s0,1000)
+    n_dim = length(u0)
 	du = rand(n_dim, n_dim)
 	du /= norm(du)
 	Q_init, R_init = qr(du)
-	tau = 10000
-	u = zeros(n_dim,tau)
+	t = 1000
+	tau = 600
+	u = zeros(n_dim,t)
 	u[:,1] = copy(u0)
-	for n = 2:tau
+	for n = 2:t
 		u[:,n] = Step(u[:,n-1],s0,1)
-    end
-	Q = SharedArray(zeros(n_dim, n_dim, tau))
+	end
+	Q = SharedArray(zeros(n_dim, n_dim, t))
 	Q[:,:,1] = Q_init
-	R = SharedArray(zeros(n_dim, n_dim, tau))
+	R = SharedArray(zeros(n_dim, n_dim, t))
 	R[:,:,1] = R_init
 	lyap_exps = zeros(3)
-	C = Matrix{Float64}(I,3,3)
-    for n = 2:tau
+	C = Matrix{Float64}(I,3,1)
+    for n = 2:t
 		@sync @distributed for k = 1:n_dim
 					Q[:,k,n] = tangent_step(Q[:,k,n-1], 
 								u[:,n-1], s0, zeros(3))
 		end
 		Q[:,:,n], R[:,:,n] = qr(Q[:,:,n])
-		lyap_exps += log.(abs.(diag(R[:,:,n])))/(tau*dt)
+		lyap_exps += log.(abs.(diag(R[:,:,n])))/(t*dt)
     end
-	for n = tau:-1:2
+	for n = t:-1:100
 		C = \(R[:,:,n], C)
+		C = C'.\diag(C)
+		C = C'
+		println(C[:,1]')
     end
-	return lyap_exps, C
+	return lyap_exps, C, Q, R
 end
